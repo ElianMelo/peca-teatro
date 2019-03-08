db27803f9f80ce741853b8730a139618e69a0ac2
ElianMelo <elianmelomorais@gmail.com>
2019-03-08 17:53:12 -0300

    Adiciona a base da classe PecaTeatro

---
+++ src/PecaTeatro.java
@@ -0,0 +1,15 @@
+/*
+Nome do autor: Elian Melo Morais
+Data de criação do arquivo: 08/03/2019
+Objetivo sucinto do programa: Simular os relacionamentos e o funcionamento de uma peça em sua totalidade
+Referência ao enunciado/origem do exercício: https://docs.google.com/document/d/1ewS7W5Lacoxjorj8NxD3zc2uH32P5fWOu2xqjVTWym0/edit
+*/
+public class PecaTeatro {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("[ Teatro Nacional ]");
+		
+	}
+
+}
